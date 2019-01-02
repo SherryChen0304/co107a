1838905fc6688c8947687c5cac776a0710f46a74
SherryChen0304 <dearsherrychen@gmail.com>
2019-01-03 07:14:02 +0800

    PC.hdl

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -6,11 +6,20 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
-   @2
+// #include <stdio.h>
+// int main() 
+// {
+//     int R0 = 3;
+//     int R1 = 5;
+//    =>    int R2 = 0;
+// }
+
+    @2
     M=0     // R2 = 0
+
     @i
     M=0     // i=0
+    
 (LOOP)
     @i
     D=M     // D=i
@@ -23,8 +32,10 @@
     D=M     // D=R1
     @2
     M=D+M   // R2=R2+R1
+
     @i
     M=M+1   // i=i+1
+
     @LOOP
     0;JMP   // Repeat
 (END)
